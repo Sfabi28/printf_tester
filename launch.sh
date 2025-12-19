5570c883cc26bc3693e79ba1d731e391a76d252e
Sfabi28 <samuelefabi28@gmail.com>
2025-12-20 00:49:34 +0100

    first push

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE_PATH="../"
+
+# Colori aggiornati per matchare Python (Serie 9x invece di 3x)
+GREEN='\033[92m'
+RED='\033[91m'
+CYAN='\033[96m'
+YELLOW='\033[93m'
+RESET='\033[0m'
+MAGENTA='\033[95m'
+BLUE='\033[94m'
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ft_printf.h"
+
+int main(int argc, char **argv)
+{
+
+}
